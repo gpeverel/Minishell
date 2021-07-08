9369f66f04c6b435d68114e877ab2e6cea85399c
foodmen <foodmen243@yandex.ru>
2021-07-08 15:42:41 +0300

    добавил функцию exit

--- build_in/ft_exit.c
+++ build_in/ft_exit.c
@@ -1,17 +1,34 @@
 #include "../minishell.h"
 
+void ft_check_letter(char *args)
+{
+	int i;
+
+	i = 0;
+	while (args[i])
+	{
+		if (ft_isdigit(args[i]) == 0)
+		{
+			printf("exit: fd: numeric argument required\n");
+			exit(255);
+		}
+		i++;
+	}
+}
+
 void	ft_exit(char **args)
 {
 	int i;
 	int	code;
 
 	i = 0;
+	ft_check_letter(args[0]);
 	code = ft_atoi(args[0]) % 256;
-	printf("code= %d\n", code);
 	while (args[i] != NULL)
 		i++;
 	if (i > 1)
 	{
+		errno = 1;
 		printf("exit: too many arguments\n");
 	}
 	else
